b4e77691b7eee73875313e000fc75b4ce342cd06
Laxmi <lakshminarayanverma91@gmail.com>
2026-03-09 04:32:41 +0530

    Implemented CRUD Operations in Student Management System

--- student/views.py
+++ student/views.py
@@ -15,19 +15,43 @@ def student_home(request):
 def add_student(request):
 
     if request.method == "POST":
-        student_name = request.POST.get("input_name")
-        student_email = request.POST.get("input_email")
-        student_phone = request.POST.get("input_phone")
-        student_image = request.FILES.get("input_image")
 
         Student.objects.create(
-            name = student_name,
-            email = student_email,
-            phone_no = student_phone,
-            image = student_image
+            name = request.POST.get("input_name"),
+            email = request.POST.get("input_email"),
+            phone_no = request.POST.get("input_phone"),
+            image = request.FILES.get("input_image")
         )
 
-        return redirect("/student/std/")
+        return redirect("student_home")
     
 
     return render(request, "student/add_student.html")
+
+def delete_student(request, student_id):
+
+    my_student = Student.objects.get(id = student_id)   # 1st id is the id that is present in model (db)
+                                                        # 2nd id is the id that comes from html
+    my_student.delete()
+
+    return redirect("student_home")
+
+def update_student(request, student_id):
+    
+    student = Student.objects.get(id = student_id)
+
+    if request.method == "POST":
+        
+        student.name = request.POST.get("input_name")
+        student.email = request.POST.get("input_email")
+        student.phone_no = request.POST.get("input_phone")
+
+        student.save()
+
+        return redirect("student_home")
+    
+    parameters = {
+        "student":student
+    }
+
+    return render(request, "student/update_student.html", parameters)
